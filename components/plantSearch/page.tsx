6721573b40a0f562eeaeaecf3fe8946f116b2aa2
assaf21 <assaf.vardy@gmail.com>
2023-05-26 19:16:47 +0300

    Search component start

--- components/plantSearch/page.tsx
+++ components/plantSearch/page.tsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import styles from "../Input.module.css";
 
@@ -6,31 +7,47 @@ const PlantSearch = () => {
   const [family, setFamily] = useState("");
 
   return (
-    <div className="flex">
-      <form >
-        <label>
+    <div className="flex justify-between">
+      <form className="flex flex-col text-left ml-12">
+        <TextField
+          className="rounded-lg"
+          id="outlined-basic"
+          label="Plant Name"
+          variant="outlined"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        >
+          {name}
+        </TextField>
+        <label className="px-6 pt-4">
           Plant Name
           <input
-            type="text"
+            name="plantName"
             value={name}
             className={styles.inputSearch}
             onChange={(event) => {
-                setName(event.target.value);
+              setName(event.target.value);
             }}
           />
         </label>
-        <label>
+        <label className="px-6 pt-4">
           Plant Family
           <input
             type="text"
             value={family}
+            name="plantFamily"
             className={styles.inputSearch}
             onChange={(event) => {
-                setFamily(event.target.value);
+              setFamily(event.target.value);
             }}
           />
         </label>
+        <Button>Click</Button>
       </form>
+      <div className="flex mr-12 pt-4">
+        <h3>Previous Search</h3>
+      </div>
     </div>
   );
 };
