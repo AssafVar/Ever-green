ff334ec02d9015ccba97376ee1b105ca02b9ede2
assaf21 <assaf.vardy@gmail.com>
2023-05-28 00:42:34 +0300

    search checkboxes

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,21 @@ import Layout from "@/components/layout/layout";
 import Novels from "@/components/novels/page";
 import { Providers } from "@/components/Providers";
 import PlantSearch from "@/components/plantSearch/page";
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css';
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import { Container } from "@mui/material";
 
 export default function Home() {
   return (
     <main>
       <Providers>
         <Layout home={true}>
-        <PlantSearch/>
-        <Novels />
+          <Container>
+            <PlantSearch />
+            <div className="border-b-4 border-gray-400"></div>
+            <Novels />
+            
+          </Container>
         </Layout>
       </Providers>
     </main>
--- components/plantSearch/page.tsx
+++ components/plantSearch/page.tsx
@@ -1,54 +1,98 @@
-import { Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControlLabel,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
-import styles from "../Input.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import Checkbox from "@mui/material/Checkbox";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const PlantSearch = () => {
   const [name, setName] = useState("");
   const [family, setFamily] = useState("");
+  const [checkCommonName, setCheckCommonName] = useState(true);
+  const [checkFamily, setCheckFamily] = useState(true);
 
   return (
-    <div className="flex justify-between">
-      <form className="flex flex-col text-left ml-12">
-        <TextField
-          className="rounded-lg"
-          id="outlined-basic"
-          label="Plant Name"
-          variant="outlined"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        >
-          {name}
-        </TextField>
-        <label className="px-6 pt-4">
-          Plant Name
-          <input
-            name="plantName"
-            value={name}
-            className={styles.inputSearch}
-            onChange={(event) => {
-              setName(event.target.value);
-            }}
+    <div className="flex flex-row">
+      <div className="flex-column basis-3/4 mr-10">
+        <Box>
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={(e) => setCheckCommonName(e.target.checked)}
+                checked={checkCommonName}
+              />
+            }
+            label="Common name"
           />
-        </label>
-        <label className="px-6 pt-4">
-          Plant Family
-          <input
-            type="text"
-            value={family}
-            name="plantFamily"
-            className={styles.inputSearch}
-            onChange={(event) => {
-              setFamily(event.target.value);
-            }}
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={(e) => setCheckFamily(e.target.checked)}
+                checked={checkFamily}
+              />
+            }
+            label="Common name"
           />
-        </label>
-        <Button>Click</Button>
-      </form>
-      <div className="flex mr-12 pt-4">
-        <h3>Previous Search</h3>
+        </Box>
+        <Box>
+          {checkCommonName && (
+            <TextField
+              className="mt-5"
+              id="outlined-basic"
+              label="Plant Name"
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                style: {
+                  borderRadius: "10px",
+                },
+              }}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              value={name}
+            />
+          )}
+          {checkFamily && (
+            <TextField
+              className="mt-2"
+              id="outlined-basic"
+              label="Plant Family"
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                style: {
+                  borderRadius: "10px",
+                },
+              }}
+              onChange={(event) => {
+                setFamily(event.target.value);
+              }}
+              value={family}
+            />
+          )}
+          <div className="flex my-4 ">
+            <Tooltip title="Search for your plant">
+              <Button variant="text" className="ml-auto mr-6 ">
+                <SearchIcon />
+              </Button>
+            </Tooltip>
+          </div>
+        </Box>
+      </div>
+      <div className="flex mr-12 pt-4 basis-1/4 border-2 my-10 ml-5 rounded-lg border-green-700">
+        <h3 className="px-4"> Previous Search</h3>
       </div>
     </div>
   );
 };
+
 export default PlantSearch;
