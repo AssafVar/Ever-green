13c56a15ce5f63bc55df4f27064996c26b270328
assaf21 <assaf.vardy@gmail.com>
2023-06-06 23:09:49 +0300

    redesign search bar

--- components/plantSearch/SearchBar.tsx
+++ components/plantSearch/SearchBar.tsx
@@ -1,7 +1,8 @@
+import React, { useState } from "react";
 import {
   Box,
   FormControl,
-  IconButton,
+  InputAdornment,
   InputLabel,
   MenuItem,
   Select,
@@ -9,9 +10,9 @@ import {
   TextField,
   Tooltip,
 } from "@mui/material";
-import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useMutation } from "@apollo/client";
 import { INSERT_SEARCH } from "@/graphql/queries";
 import { nanoid } from "nanoid";
@@ -24,40 +25,39 @@ const searchItems = [
 ];
 
 type SearchBarProps = {
-  updateSearchList: (item:Search|string, action:string)=> void
-}
+  updateSearchList: (item: Search | string, action: string) => void;
+};
 
 const SearchBar = ({ updateSearchList }: SearchBarProps) => {
-
   const [name, setName] = useState("");
   const [searchItem, setSearchItem] = useState({
     name: searchItems[0].name,
     value: searchItems[0].value,
   });
 
-const [insertSearch, { loading, error, data }] = useMutation(INSERT_SEARCH);
+  const [insertSearch, { loading, error, data }] = useMutation(INSERT_SEARCH);
 
-const handleNewSearch = async (item:{value:string, name:string}) => {
-  try{
-    const variables = {
-      id: nanoid(),
-      userId:'12',
-      searchCode: item.value,
-      searchString: item.name,
-      createAt: (new Date()).toLocaleString(),
+  const handleNewSearch = async (item: { value: string; name: string }) => {
+    try {
+      const variables = {
+        id: nanoid(),
+        userId: "12",
+        searchCode: item.value,
+        searchString: item.name,
+        createAt: new Date().toLocaleString(),
+      };
+      const response: any = await insertSearch({
+        variables: variables,
+      }).then(() => {
+        updateSearchList(variables, "insert");
+      });
+    } catch (err) {
+      console.log(err);
     }
-    const response:any = await insertSearch({
-      variables:variables,
-    }).then(()=>{
-      updateSearchList(variables,'insert')
-    })
-  }catch(err) {
-    console.log(err);
-  }
-}
+  };
 
   const fetchPlant = async () => {
-    handleNewSearch({value: searchItem.value, name})
+    handleNewSearch({ value: searchItem.value, name });
   };
 
   const handleSearchChange = (event: SelectChangeEvent) => {
@@ -66,32 +66,11 @@ const handleNewSearch = async (item:{value:string, name:string}) => {
   };
 
   return (
-    <div className="flex flex-row  justify-center">
+    <div className="flex flex-row justify-center">
       <div className="flex-column">
+
         <Box className="flex items-center mt-5">
-          <FormControl>
-            <InputLabel id="select-search"></InputLabel>
-            <Tooltip title="Select your search" placement="top">
-              <Select
-                labelId="select-search"
-                id="select-search"
-                value=""
-                IconComponent={ArrowDropDownCircleIcon}
-                onChange={handleSearchChange}
-                sx={{
-                  "& fieldset": { border: "none" },
-                }}
-              >
-                {searchItems.map((item) => {
-                  return (
-                    <MenuItem key={item.key} value={JSON.stringify(item)}>
-                      {item.name}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </Tooltip>
-          </FormControl>
+  
           <TextField
             id="outlined-basic"
             label={`Search ${searchItem.name}`}
@@ -101,22 +80,51 @@ const handleNewSearch = async (item:{value:string, name:string}) => {
               style: {
                 borderRadius: "10px",
               },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <FormControl>
+                  <InputLabel id="select-search"></InputLabel>
+                  <Tooltip title="Select your search" placement="top">
+                    <Select
+                      labelId="select-search"
+                      id="select-search"
+                      value=""
+                      onChange={handleSearchChange}
+                      sx={{
+                        "& fieldset": { border: "none" },
+                      }}
+                      IconComponent={MenuIcon}
+                    >
+                      {searchItems.map((item) => {
+                        return (
+                          <MenuItem
+                            key={item.key}
+                            value={JSON.stringify(item)}
+                          >
+                            {item.name}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </Tooltip>
+                </FormControl>
+              ),
             }}
             onChange={(event) => {
               setName(event.target.value);
             }}
             onKeyDown={(event) => {
-              {
-                event.key === "Enter" && fetchPlant();
+              if (event.key === "Enter") {
+                fetchPlant();
               }
             }}
             value={name}
           />
-          <Tooltip title="Search">
-            <IconButton className="m-auto " onClick={fetchPlant}>
-              <SearchIcon />
-            </IconButton>
-          </Tooltip>
+
         </Box>
       </div>
     </div>
